1b9e49e68357d41bab7b93900b40e7ef3ca26f85
JojuGeorge <jojugeorge001@gmail.com>
2026-05-15 12:41:57 +0530

    chatmodel, prompt template, chain

--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
-load_dotenv()
+load_dotenv()   # searches for .env file and take all the env vars and load their values into our env
+
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key)
+information = """
+    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and public official known for his leadership of Tesla and SpaceX. Musk has been the wealthiest person in the world since 2025; as of May 2026, Forbes estimates his net worth to be US$788 billion.
+
+Born into the wealthy Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
+
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, Tesla approved a pay package worth $1 trillion for Musk, which he is to receive over 10 years if he meets specific goals.
+"""
+
+summary_template = """
+    Given the information {info} about a persion, create the following : 
+    1. A short summary
+    2. Give 2 interesting facts about the person
+"""
+
+prompt = PromptTemplate.from_template(summary_template)
+# final_prompt = prompt.format(info=information)    # becomes a string therefore cannot be used in chain
+
+llm = ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, temperature=0)
+
+# LCEL LangChain Expression Language (chain)
+chain = prompt | llm
 
 
 def main():
-    print("Hello from langchain-course!")
-    # response = llm.invoke("Hello there.")
-    # print(response)
-    # print(response.content)
+    response = chain.invoke({"info": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
