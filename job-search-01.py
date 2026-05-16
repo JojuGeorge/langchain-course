133d4d838cd2c81c24bbd94ed04d72b7558f40f9
JojuGeorge <jojugeorge001@gmail.com>
2026-05-16 17:22:46 +0530

    edits

--- job-search-01.py
+++ job-search-01.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 load_dotenv()
 
-# Structuring using Pydantic
+# Structure data using Pydantic
 # To represent the source of the answer
 
 
